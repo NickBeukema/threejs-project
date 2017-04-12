06fe330c0975d087e4c3f7a5ffb02642d4aacff0
Kevin <andersk82@yahoo.com>
2017-04-11 21:25:39 -0400

    added texture mapping to the ground

--- src/js/modules/game-view.js
+++ src/js/modules/game-view.js
@@ -1,9 +1,10 @@
 import { gridWidth, gridLength, gridSubDiv } from './constants';
 
-const grassTexture = new THREE.ImageUtils.loadTexture("textures/stone.jpg");
-grassTexture.repeat.set(7, 7);     // repeat the texture 6x in both s- and t- directions
-grassTexture.wrapS = THREE.RepeatWrapping;
-grassTexture.wrapT = THREE.RepeatWrapping;
+const planeTexture = new THREE.TextureLoader().load("textures/stone.jpg");
+planeTexture.repeat.set(10,10);
+planeTexture.wrapS = THREE.RepeatWrapping;
+planeTexture.wrapT = THREE.RepeatWrapping;
+
 
 export default class GameView {
   constructor(globals, gameEngine) {
@@ -16,7 +17,7 @@ export default class GameView {
     this.setupGrid();
     this.setupLight();
 
-    this.mouse = { x: 0, ady: 0 };
+    this.mouse = { x: -10000, y: 0 };
 
     let positionScale = gridWidth / 2;
 
@@ -42,7 +43,14 @@ export default class GameView {
   }
 
   setupGrid() {
-    var grid = new THREE.GridHelper(gridWidth, gridSubDiv);
+    let texturedSurfaceGeometry = new THREE.PlaneGeometry(gridWidth, gridWidth);
+    let texturedSurfaceMaterial = new THREE.MeshBasicMaterial({map: planeTexture});
+    let texturedSurface = new THREE.Mesh(texturedSurfaceGeometry, texturedSurfaceMaterial);
+    texturedSurface.rotation.x = -0.5 * Math.PI;
+    texturedSurface.position.y = -0.3;
+    this.scene.add(texturedSurface);
+
+    let grid = new THREE.GridHelper(gridWidth, gridSubDiv);
     this.scene.add(grid);
 
     this.gridPanels = {};
@@ -51,9 +59,9 @@ export default class GameView {
     let zPos = ((gridWidth / 2) - panelLength / 2) * -1;
 
     for(let i = 0; i < gridSubDiv; i++) {
-      var planeGeometry = new THREE.PlaneGeometry(gridWidth, panelLength);
-      var planeMaterial = new THREE.MeshBasicMaterial({map: grassTexture});
-      var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      let planeGeometry = new THREE.PlaneGeometry(gridWidth, panelLength);
+      let planeMaterial = new THREE.MeshBasicMaterial({transparent: true, opacity: 0});
+      let plane = new THREE.Mesh(planeGeometry, planeMaterial);
       plane.rotation.x = -0.5 * Math.PI;
       plane.position.z = zPos;
       plane.position.y = -0.2;
@@ -67,21 +75,22 @@ export default class GameView {
   checkGridIntersections() {
     this.raycaster.setFromCamera(this.mouse, this.camera);
     let planes = Object.values(this.gridPanels);
-    var intersects = this.raycaster.intersectObjects(planes);
+    let intersects = this.raycaster.intersectObjects(planes);
 
     planes.forEach((panel) => {
       panel.material.color.setHex(0x666666);
+      panel.material.opacity = 0;
     });
 
     if(intersects.length > 0) {
       let obj = intersects[0].object;
 
-      obj.material.color.setHex(0xff0000);
+      obj.material.color.setHex(0xff0000);      
+      obj.material.opacity = .5;
       this.currentHoveredPanel = obj;
     } else if(this.currentHoveredPanel){
       this.currentHoveredPanel = null;
     }
-
   }
 
   getCurrentPanelIndex() {
@@ -110,4 +119,4 @@ export default class GameView {
     this.checkGridIntersections();
     this.gameEngine.gameLoop(timestamp);
   }
-}
+} 
